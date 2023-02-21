484ee45c43a31bb03bfcc538698fd0700856441a
Konst-Is <konst.is@mail.ru>
2023-02-21 19:32:05 +0300

    added alert

--- Alert_Beliy/Alert_Beliy/ViewController.swift
+++ Alert_Beliy/Alert_Beliy/ViewController.swift
@@ -14,6 +14,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showAlert(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Photo Access", message: "Doyou alow to use your photos?", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func showActionSheet(_ sender: UIButton) {
+    }
+    
 }
 
