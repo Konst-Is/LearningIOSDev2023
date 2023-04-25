4aec2757414fcbbb26d31bed886eac4d6ab6ab51
Konst-Is <konst.is@mail.ru>
2023-04-25 22:48:41 +0300

    Added a POST request

--- Networking2_SB/Networking2_SB/ViewController.swift
+++ Networking2_SB/Networking2_SB/ViewController.swift
@@ -8,17 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func getRequest(_ sender: UIButton) {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let response = response,
-            let data = data else { return }
+                  let data = data else { return }
             do {
                 let json = try JSONSerialization.jsonObject(with: data)
                 print(json)
@@ -27,12 +27,31 @@ class ViewController: UIViewController {
             }
             
             
-
+            
         }.resume()
     }
     
     @IBAction func postRequest(_ sender: UIButton) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        let userData = ["Course" : "Networking", "Lesson" : "GET and POST Requests"]
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: userData) else { return }
+        request.httpBody = httpBody
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let response = response,
+                  let data = data else { return }
+            print(response)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data)
+                print(json)
+            } catch {
+                print(error)
+            }
+        }.resume()
     }
-    
+
 }
 
